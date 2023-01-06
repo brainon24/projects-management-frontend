fda10ec974059e398872f3b9e4e9f5d7a655c5bd
daviddiazh <daviddiazlds1221@gmail.com>
2023-01-05 20:13:03 -0500

    HTML and CSS of sign up page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import { Link as LinkRRD } from 'react-router-dom';
+import { Box, Link, Input, Typography } from '@mui/material';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+
+import '../styles/signUp.css';
 
 export const SignUp = () => {
+
+    const onSubmit = (event: any) => {
+        event.preventDefault();
+
+        console.log('click');
+    }
+
     return (
-        <div>SignUp</div>
+        <div className='container-page'>
+            <Box 
+                component='section'
+                className='go-to-back-container'
+            >
+                <LinkRRD to='/'>
+                    <Link className='go-to-back-icon'>
+                        <AiOutlineArrowLeft />
+                    </Link>
+                </LinkRRD>
+            </Box>
+
+            <Box 
+                component='section'
+                className='sign-up-page-container'
+            >
+                <form
+                    onSubmit={ onSubmit }
+                    className='form-container'
+                >
+                    <h1>Registro</h1>
+                    <Input 
+                        placeholder='Nombre'
+                        className='form-input'
+                        type='text'
+                        autoComplete='off'
+                    />
+                    <Input 
+                        placeholder='Apellidos'
+                        className='form-input'
+                        type='text'
+                        autoComplete='off'
+                    />
+                    <Input 
+                        placeholder='Correo Electrínico'
+                        className='form-input'
+                        type='email'
+                        autoComplete='off'
+                    />
+                    <Input 
+                        placeholder='Contraseña'
+                        className='form-input'
+                        type='password'
+                        autoComplete='off'
+                    />
+                    <Input 
+                        placeholder='Celular'
+                        className='form-input'
+                        type='number'
+                        autoComplete='off'
+                    />
+                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <button
+                            className='btn-sign-up'
+                        >
+                            Registrarme
+                        </button>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', justifyContent: 'space-around', margin: '2rem 0'}}>
+                        <hr style={{ width: '50%', height: '2px', margin: 'auto 0' }} />
+                        <Typography sx={{padding: '0 .5rem'}}>O</Typography>
+                        <hr style={{ width: '50%', height: '2px', margin: 'auto 0' }} />
+                    </Box>
+                    
+                    <LinkRRD to='/login'>
+                        <p className='login-link'>¿Ya tienes cuenta? ¡Inicia sesión!</p>
+                    </LinkRRD>
+                </form>
+            </Box>
+        </div>
     );
 }
