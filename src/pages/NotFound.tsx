0766de12c0663d35d8903eb0daa224f967a07921
daviddiazh <daviddiazlds1221@gmail.com>
2023-01-08 20:54:49 -0500

    Not found page ok

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Box } from '@mui/material';
+import logo from '../assets/logo-brainon24.jpeg';
+import { Link as LinkRRD } from 'react-router-dom';
+
+import '../styles/notFound.css';
 
 export const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <Box className='container-not-found-page'>
+            <Box>
+                <Box className='img-logo'>
+                    <img 
+                        src={logo} 
+                        alt="Logo de brainon24" 
+                        width={180}
+                    />
+                </Box>
+                <h1 className='title'>Upss!</h1>
+                <p className='text'>Esta página no fue encontrada, haz abajo para volver al home!</p>
+                <LinkRRD to='/private' style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button className='btn-login'>Ir al home</button>
+                </LinkRRD>
+            </Box>
+        </Box>
     )
 }
