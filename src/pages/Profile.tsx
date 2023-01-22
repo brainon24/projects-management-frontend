a7b7f76fe34196bb97824283811f4714dee1c94a
daviddiazh <daviddiazlds1221@gmail.com>
2023-01-22 18:41:58 -0500

    Profile page in BETA Mode

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,19 @@
 import { MainLayout } from "../layouts/MainLayout"
+import {useSelector} from "react-redux";
 
 export const Profile = () => {
+
+    const { user } = useSelector((state: any) => state.auth);
+
     return (
         <MainLayout>
-            <div>Profile</div>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: 100 }}>
+                <img src={ user.profilePicture } alt="Foto de perfil" width={170} style={{ borderRadius: 100, marginRight: 50 }}/>
+                <div>
+                    <h1 style={{ textAlign: 'center' }}>{ user.fullName }</h1>
+                    <p style={{ textAlign: 'center' }}> { user.role }: { user.business.businessName }</p>
+                </div>
+            </div>
         </MainLayout>
-    )
+    );
 }
