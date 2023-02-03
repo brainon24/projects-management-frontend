22fc06c895fc04964f82e8f6d906e97896eb6b18
daviddiazh <daviddiazlds1221@gmail.com>
2023-02-03 09:18:46 -0500

    Modal for view user with details ok

--- src/components/UserModal.tsx
+++ src/components/UserModal.tsx
@@ -2,98 +2,9 @@ import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, F
 import { TransitionProps } from "@mui/material/transitions";
 import { Box } from "@mui/system";
 import React from "react";
+import { RiUser6Line } from 'react-icons/ri';
 import { useDispatch } from "react-redux";
-import { useNavigate } from 'react-router-dom';
 import { clearUserReducer } from "../store/users/usersSlice";
-
-// export default function UserModal({ user }: any) {
-//   const [open, setOpen] = useState(true);
-
-//     const typeDocuments = [
-//         "USER",
-//         "CLIENT",
-//         "EMPLOYEE",
-//         "ADMIN",
-//     ];
-
-//     const [typeDoc, setTypeDoc] = useState(user.role);
-
-//     const onTypeDocumentChanged = (event: any) => { 
-//         setTypeDoc(event.target.value);
-//     };
-        
-//     const dispatch = useDispatch();
-
-//     const handleClose = () => {
-//         setOpen(false);
-        
-//         dispatch( clearUserReducer() );
-//     };
-
-//     console.log(user)
-
-//     return (
-//         <div 
-//             style={{
-//                 paddingTop: 15
-//             }}
-//         >
-//             <Dialog 
-//                 open={open} 
-//                 onClose={handleClose} 
-//                 sx={{ backdropFilter: 'blur(3px)', bgcolor: 'rgba(0,0,0, 0.2)', paddingX: 100 }}
-//             >
-//                 <Box 
-//                     sx={{ 
-//                         textAlign: 'center', 
-//                     }}
-//                 >
-//                     <DialogTitle>Actualizar Rol del Usuario</DialogTitle>
-//                     <DialogContent sx={{ 
-//                             padding: '20px 100px', 
-//                         }}>
-//                         <DialogContentText>
-//                             {user.fullName} - {user.businessName}
-//                         </DialogContentText>
-
-//                         <Grid item xs={12} style={{
-//                             width: '100%',
-//                             paddingTop: 15
-//                         }}>
-//                             <FormControl fullWidth sx={{ mb: 2 }} variant="outlined">
-//                                 <InputLabel id="demo-simple-select-label">{ typeDoc }</InputLabel>
-//                                 <Select
-//                                     labelId="demo-simple-select-label"
-//                                     id="demo-simple-select"
-//                                     value={typeDoc}
-//                                     label="Tipo documento"
-//                                     onChange={onTypeDocumentChanged}
-//                                     color='info'
-                                    
-//                                 >
-//                                     {
-//                                         typeDocuments.map(typeDocument => (
-//                                             <MenuItem
-//                                                 key={typeDocument}
-//                                                 value={typeDocument}
-//                                             >
-//                                                 {typeDocument}
-//                                             </MenuItem>
-//                                         ))
-//                                     }
-//                                 </Select>
-//                             </FormControl>
-//                         </Grid>
-//                     </DialogContent>
-//                     <DialogActions>
-//                         <Button onClick={handleClose}>Cancelar</Button>
-//                         <Button onClick={handleClose}>Guardar</Button>
-//                     </DialogActions>
-//                 </Box>
-//             </Dialog>
-//         </div>
-//     );
-// }
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
@@ -154,36 +65,58 @@ export const UserModal = ({ user }: any) => {
                   aria-describedby="alert-dialog-slide-description"
                   sx={{ backdropFilter: 'blur(3px)', bgcolor: 'rgba(0,0,0, 0.2)' }}
               >
+                <DialogTitle
+                          sx={{
+                              color: 'var(--dark)',
+                              fontWeight: 600,
+                              textAlign: 'left'
+                          }}
+                      >
+                          Actualizar Rol del Usuario
+                      </DialogTitle>
                   <Box
                       sx={{ 
                           textAlign: 'center', 
-                          margin: '10px 30px' 
+                          margin: '10px 30px',
                       }}
                   >
-                      {/* <Box style={{
-                          backgroundColor: 'var(--green)',
+                      <Box style={{
+                          backgroundColor: 'var(--gray)',
                           borderRadius: '100%',
                           width: 35,
                           height: 35,
-                          margin: '30px auto 10px',
-                          display: 'flex',                        
+                          margin: '20px auto -10px',
+                          display: 'flex',
                       }}>
                           <Box style={{
                               margin: '8.5px auto',
                           }}>
-                              <FiCheck style={{
-                                  color: '#fff',
+                              <RiUser6Line style={{
+                                  color: '#000',
                               }} />
                           </Box>
-                      </Box> */}
+                      </Box>
                       <DialogTitle
                           sx={{
                               color: 'var(--dark)',
-                              fontWeight: 600
+                              fontWeight: 600,
                           }}
                       >
-                          { user.fullName }
+                          { user.fullName } · { user.businessName }
                       </DialogTitle>
+                      <DialogContentText
+                              id="alert-dialog-slide-description"
+                              style={{
+                                  textAlign: 'center',
+                                  fontSize: 13,
+                                  marginTop: -15
+                              }}
+                        >
+                              <span style={{
+                                fontWeight: 500
+                              }}>Registro en la plataforma:</span> { user.createdAt }
+                          </DialogContentText>
+
   
                       <DialogContent
                           sx={{ 
@@ -193,7 +126,7 @@ export const UserModal = ({ user }: any) => {
                           <DialogContentText
                               id="alert-dialog-slide-description"
                               style={{
-                                  textAlign: 'center',
+                                  textAlign: 'left',
                                   fontSize: 13,
                                   paddingBottom: 15
                               }}
@@ -201,7 +134,7 @@ export const UserModal = ({ user }: any) => {
                               Rol Actual.
                           </DialogContentText>
                           <FormControl fullWidth sx={{ mb: 2 }} variant="outlined">
-                                 <InputLabel id="demo-simple-select-label">{ typeDoc }</InputLabel>
+                                 <InputLabel id="demo-simple-select-label">{ user.role }</InputLabel>
                                  <Select
                                     labelId="demo-simple-select-label"
                                     id="demo-simple-select"
@@ -209,7 +142,6 @@ export const UserModal = ({ user }: any) => {
                                     label="Tipo documento"
                                     onChange={onTypeDocumentChanged}
                                     color='info'
-                                    
                                 >
                                     {
                                         typeDocuments.map(typeDocument => (
@@ -224,9 +156,48 @@ export const UserModal = ({ user }: any) => {
                                 </Select>
                             </FormControl>
 
+                            <DialogContentText
+                              id="alert-dialog-slide-description"
+                              style={{
+                                  textAlign: 'center',
+                                  fontSize: 13,
+                                  marginTop: 15
+                              }}
+                            >
+                                <span style={{
+                                    fontWeight: 500
+                                }}>ID del Usuario:</span> { user._id }
+                            </DialogContentText>
+
+                            <DialogContentText
+                              id="alert-dialog-slide-description"
+                              style={{
+                                  textAlign: 'center',
+                                  fontSize: 13,
+                                  marginTop: 15
+                              }}
+                            >
+                                <span style={{
+                                    fontWeight: 500
+                                }}>ID del Negocio:</span> { user.businessId }
+                            </DialogContentText>
+
                       </DialogContent>
   
                       <DialogActions>
+                        <button 
+                              onClick={handleClose}
+                              style={{
+                                  padding: '8px 20px',
+                                  borderRadius: 6,
+                                  border: 'none',
+                                  backgroundColor: 'var(--red)',
+                                  color: 'var(--white)',
+                                  cursor: 'pointer',
+                              }}
+                          >
+                              Cancelar
+                          </button>
                           <button 
                               onClick={handleClose}
                               style={{
