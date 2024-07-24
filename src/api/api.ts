e0f34e4aa04c5371d3de6d1f91c8ae20d03a90ae
daviddiazh <daviddiazlds1221@gmail.com>
2024-07-23 20:29:54 -0500

    fix: app fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-export const baseURL = 'https://gestor-proyectos-4e8c263c4a4a.herokuapp.com';
+export const baseURL = 'https://projects-management-backend-six.vercel.app';
 // export const baseURL = 'http://localhost:8082';
 
 const projectsManagement = axios.create({ baseURL });
