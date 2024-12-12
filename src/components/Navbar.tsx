5481fbf862e5bb2232955eef73e67791d222d324
daviddiazh <daviddiazlds1221@gmail.com>
2024-12-11 19:33:26 -0500

    feat: new change inside Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,9 @@ import { openSidemenu } from "../store/ui/uiSlice";
 import { BiMenu } from 'react-icons/bi'
 
 import './styles/navbar.css';
-import { Link, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Link as LinkRRD } from 'react-router-dom';
+import logo from '../assets/brainon24-logo.png'
 
 export const Navbar = () => {
 
@@ -15,7 +16,8 @@ export const Navbar = () => {
         <div className="container-navbar">
             <LinkRRD to={ user ? '/private' : '/' } style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
                 <Typography variant='h6' color='var(--blue)'>Gestor Proyectos |</Typography>
-                <Typography sx={{ ml: 0.5 }}>brainon24</Typography>
+                <img src={ logo } alt='Logo' width={100} style={{ marginLeft: 6 }} />
+                {/* <Typography sx={{ ml: 0.5 }}>brainon24</Typography> */}
             </LinkRRD> 
 
             
